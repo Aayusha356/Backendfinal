2994c14902294844b357843cc87ce6db43c3daf7
Sadiksha1 <078bct028.sadiksha@sagarmatha.edu.np>
2025-01-27 18:50:04 +0545

    media\products

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -8,15 +9,34 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    ]
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-
+    size = models.CharField(max_length=2, choices=SIZE_CHOICES, null=True, blank=True)
+    
+    
     def __str__(self):
         return self.name
+    
+class Rating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)  # Use custom user model
+    score = models.PositiveIntegerField(
+        choices=[(i, f"{i} star{'s' if i > 1 else ''}") for i in range(1, 6)], 
+        default=5
+    )  # Ratings from 1 to 5 stars
+    review = models.TextField(blank=True, null=True)  # Optional review text
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.score} by {self.user or 'Anonymous'} for {self.product.name}"
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CategoryViewSet, CustomerViewSet, CartViewSet, OrderViewSet
 
@@ -14,6 +13,3 @@ router.register('orders', OrderViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
